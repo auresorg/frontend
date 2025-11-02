d3f6b833231a4299cc408aad08b6e8f1a850ef11
mvishok <vishokmanikantan@gmail.com>
2025-11-02 17:07:06 +0530

    refactor: adjust indentation in next.config.mjs for consistency

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',  // enable standalone SSR output
-  images: {
-    domains: ['avatars.githubusercontent.com'],
-  },
+    images: {
+        domains: ['avatars.githubusercontent.com'],
+    },
 };
 
 export default nextConfig;
