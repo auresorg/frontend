bce0140c718dd9a3e95552a85581b3180c2e1721
harsh12bhardwaj <hbhardwaj283@gmail.com>
2026-01-31 16:22:45 +0530

    added instagram link and icon

--- src/components/ui/landing/footer.tsx
+++ src/components/ui/landing/footer.tsx
@@ -21,7 +21,9 @@ function Footer() {
                     <a className="size-8 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent text-gray-500 hover:bg-gray-50 focus:outline-hidden focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:text-neutral-400 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700" href="https://github.com/auresorg" target='blank'>
                         <RiGithubFill className="size-6" />
                     </a>
-                    {/* COPY PASTE AND IMPLEMENT INSTAGRAM ICON */}
+                    <a className="size-8 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent text-gray-500 hover:bg-gray-50 focus:outline-hidden focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:text-neutral-400 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700" href="https://instagram.com/madebyaures" target='blank'>
+                        <RiInstagramFill className="size-6" />
+                    </a>
                     
                 </div>
                 {/* End Social Brands */}
